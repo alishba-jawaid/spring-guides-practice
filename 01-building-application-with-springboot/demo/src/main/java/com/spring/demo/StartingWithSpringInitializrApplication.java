1bab0efa696a989b3fc079311f9732aabe739880
Alishba Jawaid <alishba.jawaid96@gmail.com>
2025-10-17 14:39:01 +0200

    initial

---
+++ 01-building-application-with-springboot/demo/src/main/java/com/spring/demo/StartingWithSpringInitializrApplication.java
@@ -0,0 +1,14 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingWithSpringInitializrApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(StartingWithSpringInitializrApplication.class, args);
+	}
+
+}
